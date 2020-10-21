f98965eea626eac825dba2d8a36ec65240bc795b
vidyaelluru <vidyaelluru@gmail.com>
2020-10-21 20:51:27 +0530

    first docker image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,16 @@
+
+# set the base image
 FROM openjdk:8
+
+#add resources to the container
+VOLUME /tmp
+
 EXPOSE 8080
-ADD target/helloworld-springboot-0.0.1-SNAPSHOT.jar helloworld-springboot-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java", "-jar", "helloworld-springboot-0.0.1-SNAPSHOT.jar"]
+# add scripts to the container
+ADD target/helloworldspring.jar helloworldspring.jar
+
+# install app runtimes and modules
+RUN bash -c 'touch /helloworldspring.jar'
+RUN apt-get update
+
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom", "-jar", "/helloworldspring.jar"]
--- src/main/java/com/example/helloworldspringboot/HelloworldSpringbootApplication.java
+++ src/main/java/com/example/helloworldspringboot/HelloworldSpringbootApplication.java
@@ -1,21 +1,31 @@
 package com.example.helloworldspringboot;
 
+import jdk.nashorn.internal.runtime.logging.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * below are the annotations used to call appropriate packages, classes and its relevant methods
+ */
 @RestController
 @SpringBootApplication
+@Logger
 public class HelloworldSpringbootApplication
 {
-    @GetMapping("/message")
+	//private static final Logger LOGGER=LoggerFactory.getLogger(HelloworldSpringbootApplication.class);
+
+	@GetMapping("/helloworld")
 	public String getMessage()
 	{
 		return "Hello Springboot World ....!!!";
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(HelloworldSpringbootApplication.class, args);
+		/*LOGGER.info("Simple log statement with inputs {}, {} and {}", 1,2,3);*/
 	}
 
 }
